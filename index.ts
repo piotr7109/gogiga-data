bacd7d4c31460d0f0c91860391c8bd3c8c568dd6
piotr7109 <piotrkrk83@gmail.com>
2025-10-26 20:04:21 +0100

    Update typescript config and import policy

--- index.ts
+++
@@ -1,3 +0,0 @@
-export { cities, City, Point } from "./src/cities";
-export { countries, Country } from "./src/countries";
-export { Genre, genres } from "./src/genres";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { cities, City, Point } from "./cities";
+export { countries, Country } from "./countries";
+export { Genre, genres } from "./genres";
